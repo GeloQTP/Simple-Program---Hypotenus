32fef43f94c877bea86f8830ba745cd8ce88d02b
Gelo <geloadrian24@gmail.com>
2023-03-16 17:54:08 +0800

    Add project files.

---
+++ bro code/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(String[] Args)
+    {
+        Console.WriteLine("Enter Side A: ");
+        double Side_A = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Enter Side B:");
+        double Side_B = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n");
+            
+        double Ans = Math.Sqrt((Side_A * Side_A) + (Side_B * Side_B));
+
+        Console.WriteLine("The Hypotenus is: " + Ans);
+    }
+}
